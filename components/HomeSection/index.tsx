0af9e6d837fcce5847cf8c56ab60182078c22531
Adriano Ribeiro <adrianotribeiro092@gmail.com>
2023-08-04 17:10:11 -0300

    feat/NIS-01 - termino da primeira section assim como  sua responsividade

--- components/HomeSection/index.tsx
+++ components/HomeSection/index.tsx
@@ -3,13 +3,13 @@ import React from "react"
 import KnowMore from "../KnowMore"
 export default function HomeSection(): React.ReactElement {
   return (
-    <section className="p-container py-12 md:h-fit lg:justify-between xl:flex xl:h-[90vh] xl:items-center xl:py-[0.20rem] h-xl:h-[91.6vh] lg-xl:h-[91.1vh] sm-2xl:h-[89.6vh]">
+    <section className="py-12 md:h-fit lg:justify-between xl:flex xl:h-[90vh] xl:items-center xl:py-[0.20rem] h-xl:h-[91.6vh] lg-xl:h-[91.1vh] sm-2xl:h-[89.6vh] 3xl:h-[92.4vh] 4xl:h-[92.6vh]">
       <div className="hidden xl:inline xl:overflow-hidden">
         <Image
           width={841}
           height={830}
           alt="Horse Image"
-          className="r-img min-[1920px]:w-[903px] xl:w-[660px] 2xl:w-[1039px] h-xl:absolute h-xl:-left-40 h-xl:top-16 h-xl:w-[800px] lg-xl:w-[760px] sm-2xl:w-[620px] "
+          className="r-img xl:w-[660px] 2xl:w-[1039px] h-xl:absolute h-xl:-left-40 h-xl:top-16 h-xl:w-[800px] lg-xl:w-[760px] sm-2xl:w-[620px] 3xl:absolute 3xl:left-[-8rem] 3xl:top-[4rem] 3xl:w-[890px] 4xl:left-[-4rem] 4xl:w-[917px]"
           src="/images/principal_horse_image-fotor.png"
         />
       </div>
@@ -25,25 +25,25 @@ export default function HomeSection(): React.ReactElement {
             />
           </div>
           <div className="flex flex-col items-center pb-4 xl:pb-0">
-            <p className="princpal-colors home-title w-[92%] text-center text-[1.7rem] md:text-[2rem] lg:text-[2.5rem] xl:w-[100%] xl:text-[2rem] xl:leading-8 2xl:text-[3rem] 2xl:leading-10 lg-xl:w-[80%] lg-xl:text-[2.25rem]">
+            <p className="princpal-colors w-[92%] text-center text-[1.7rem] md:text-[2rem] lg:text-[2.5rem] xl:w-[100%] xl:text-[2rem] xl:leading-8 2xl:text-[3rem] 2xl:leading-10 lg-xl:w-[80%] lg-xl:text-[2.25rem] 3xl:w-[100%]">
               Lorem Ipsum dolor sit amet, consectetur adispiscing elit.
             </p>
-            <p className="st-r princpal-colors home-subtitle hidden md:flex md:pt-3 md:text-[1.3rem] lg:text-[1.7rem] xl:pt-0 xl:text-[1.3rem]">
+            <p className="st-r princpal-colors hidden md:flex md:pt-3 md:text-[1.3rem] lg:text-[1.7rem] xl:pt-0 xl:text-[1.3rem] 3xl:text-[1.8rem]">
               Lorem Ipsum dolor sit amet, consectetur adispiscing elit.
             </p>
           </div>
           <div className="flex flex-col items-center gap-3">
-            <p className="content w-[80%] text-justify text-[1.3rem] tracking-tight text-white md:text-center md:text-[1.8rem] lg:text-[2rem] xl:w-[90%] xl:text-justify xl:text-[1.4rem]">
+            <p className="w-[80%] text-justify text-[1.3rem] tracking-tight text-white md:text-center md:text-[1.8rem] lg:text-[2rem] xl:w-[90%] xl:text-justify xl:text-[1.4rem] 3xl:text-[2rem]">
               Lorem ipsum dolor si t amet, consectetur adipiscing elit. Mauris fermentum, arcu fermentum semper
               efficitur, risus quam imperdiet neque, sit amet ullamcorper purus eros vitae nunc
             </p>
-            <p className="content w-[80%] text-justify text-[1.3rem] tracking-tight text-white md:text-center md:text-[1.8rem]  lg:text-[2rem] xl:w-[90%] xl:text-justify xl:text-[1.4rem]">
+            <p className="w-[80%] text-justify text-[1.3rem] tracking-tight text-white md:text-center md:text-[1.8rem] lg:text-[2rem]  xl:w-[90%] xl:text-justify xl:text-[1.4rem] 3xl:text-[2rem]">
               Lorem ipsum dolor si t amet, consectetur adipiscing elit. Mauris fermentum, arcu fermentum semper
               efficitur, risus quam imperdiet neque, sit amet ullamcorper purus eros vitae nunc
             </p>
           </div>
         </div>
-        <div className="k-r flex flex-col items-center gap-5 px-8 xl:flex-row lg-xl:gap-4 lg-xl:px-[2.5rem] sm-2xl:py-0">
+        <div className="flex flex-col items-center gap-5 px-8 xl:flex-row lg-xl:gap-4 lg-xl:px-[2.5rem] sm-2xl:py-0 3xl:px-11 3xl:py-4 4xl:px-12">
           <KnowMore link="#" />
           <KnowMore link="#" />
         </div>
--- components/KnowMore/index.tsx
+++ components/KnowMore/index.tsx
@@ -6,19 +6,19 @@ interface KnowMoreProps {
 
 export default function KnowMore({ link }: KnowMoreProps): React.ReactElement {
   return (
-    <div className="flex flex-col items-center lg-xl:pt-4">
-      <div>
-        <p className="kt-r princpal-colors princpal-colors text-center text-[1.8rem] md:text-[2rem] lg:text-[2.5rem] xl:text-left xl:text-[1.5rem]">
+    <div className="flex flex-col items-center lg-xl:pt-4 3xl:gap-3">
+      <div className="3xl:flex 3xl:flex-col 3xl:gap-1">
+        <p className="kt-r princpal-colors princpal-colors text-center text-[1.8rem] md:text-[2rem] lg:text-[2.5rem] xl:text-left xl:text-[1.5rem] 3xl:text-[2rem]">
           Lorem Ipsum
         </p>
-        <p className="kst-r min-[1680px]:w-[80%] min-[1680px]:leading-8 text-center text-[1.4rem] text-white md:px-12 md:text-center md:text-[1.8rem]  lg:px-[15vw] lg:text-[2rem] xl:p-0 xl:text-justify xl:text-[1.2rem] xl:leading-6 xl:tracking-tight sm-2xl:p-0 sm-2xl:text-[1.4rem] sm-2xl:leading-[1.5rem] sm-2xl:tracking-wider">
+        <p className="kst-r text-center text-[1.4rem] text-white md:px-12 md:text-center md:text-[1.8rem] lg:px-[15vw] lg:text-[2rem] xl:p-0 xl:text-justify xl:text-[1.2rem]  xl:leading-6 xl:tracking-tight lg-xl:tracking-wider sm-2xl:p-0 sm-2xl:text-[1.4rem] sm-2xl:leading-[1.5rem] sm-2xl:tracking-wider 3xl:text-[1.7rem] 3xl:leading-7 3xl:tracking-wider 4xl:text-[1.7rem] 4xl:tracking-wider">
           Lorem ipsum dolor sit amet, consectetur adipiscing eli, mauris fermentum.
         </p>
       </div>
-      <div className=" min-[1680px]:pt-4 flex w-full justify-center pt-4 lg:pt-8 xl:justify-start xl:pt-3 lg-xl:pt-4 sm-2xl:justify-start sm-2xl:pt-[0.8rem]">
+      <div className="flex w-full justify-center pt-4 lg:pt-8 xl:justify-start xl:pt-3 lg-xl:pt-4 sm-2xl:justify-start sm-2xl:pt-[0.8rem]">
         <button
           onClick={() => console.log(link)}
-          className="w-[70%] rounded bg-white py-1 text-[1.5rem] text-[#32312B] md:w-[40%] xl:w-[60%] xl:py-[0.10rem] xl:text-[1rem] hover:xl:bg-[#b6b4b4] hover:xl:text-[white] lg-xl:px-4 lg-xl:text-[1.3rem]"
+          className="w-[70%] rounded bg-white py-1 text-[1.5rem] text-[#32312B] md:w-[40%] xl:w-[60%] xl:py-[0.10rem] xl:text-[1rem] hover:xl:bg-[#b6b4b4] hover:xl:text-[white] lg-xl:px-4 lg-xl:text-[1.3rem] 3xl:text-[1.4rem]"
         >
           Lorem Ipsuma
         </button>
